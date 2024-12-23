390afe929c6bc96d5d6498e276ae8f91f8f11496
ryanthemaestro <Ryan.a.stover1@gmail.com>
2024-12-23 10:31:10 -0500

    Updated form handling for Formspree

--- js/main.js
+++ js/main.js
@@ -10,22 +10,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Handle contact form submission
 const contactForm = document.querySelector('.contact-form');
-contactForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    // Get form values
-    const name = this.querySelector('input[type="text"]').value;
-    const email = this.querySelector('input[type="email"]').value;
-    const phone = this.querySelector('input[type="tel"]').value;
-    const message = this.querySelector('textarea').value;
-    
-    // Here you would typically send this data to your server
-    // For now, we'll just show an alert
-    alert('Thank you for your message! We will get back to you soon.');
-    
-    // Clear the form
-    this.reset();
-});
+if (contactForm) {
+    contactForm.addEventListener('submit', function(e) {
+        // Let Formspree handle the submission
+        // Don't prevent default or show alert
+        // The form will be handled by Formspree
+    });
+}
 
 // Add scroll-based animations for service cards
 const observer = new IntersectionObserver((entries) => {

350166300bcf42b238537c44f17c6f4496871551
ryanthemaestro <Ryan.a.stover1@gmail.com>
2025-01-10 10:53:54 -0500

    Removed header background color change on scroll

--- js/main.js
+++ js/main.js
@@ -33,14 +33,4 @@ const observer = new IntersectionObserver((entries) => {
 
 document.querySelectorAll('.service-card').forEach((card) => {
     observer.observe(card);
-});
-
-// Update header background on scroll
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.style.background = 'rgba(255, 255, 255, 0.95)';
-    } else {
-        header.style.background = '#fff';
-    }
 }); 
